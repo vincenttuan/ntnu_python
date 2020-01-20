0066ed124b596063cbaca6ad1daa648f23470efb
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 16:02:52 +0800

    Python

--- lesson1/Hello.py
+++ lesson1/Hello.py
@@ -6,3 +6,14 @@ print(sum)
 print(a, b, sum, sep=',', end='')
 print(a, b, sum, sep=',')
 
+x = True
+y = False
+z = False
+if not x or y:
+    print(1)
+elif not x or not y and z:
+    print(2)
+elif not x or y or not y and x:
+    print(3)
+else:
+    print(4)

bde9ea97f15bc13d1c535ec89c7463e53c5369ba
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 15:02:44 +0800

    Python

--- lesson1/Hello.py
+++ lesson1/Hello.py
@@ -2,3 +2,7 @@ a = 1
 b = 2
 sum = a + b
 print(sum)
+
+studentOfAge = 18
+student_of_age = 18
+int_student_of_age = 18
---
+++ lesson1/Lotto.py
@@ -0,0 +1,7 @@
+import random
+# 四星彩
+a = random.randint(0, 9)
+b = random.randint(0, 9)
+c = random.randint(0, 9)
+d = random.randint(0, 9)
+print(a, b, c, d)

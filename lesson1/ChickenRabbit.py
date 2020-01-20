945af8494bd3f6b60c85e5a7cb78092af1e80c59
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 14:49:56 +0800

    Python

--- lesson1/ChickenRabbit.py
+++ lesson1/ChickenRabbit.py
@@ -1,9 +1,16 @@
+'''
 amount = 83
 feet = 240
-'''
 83 * 4 - 240 = 92
 92 / 2 = 46 雞
 83 - 46 = 37 兔
 '''
+def calc(amount, feet):
+    chicken = (amount * 4 - feet) / 2
+    rabbit = amount - chicken
+    print("雞: %d 兔: %d" % (chicken, rabbit))
+
+calc(83, 240)
+calc(60, 180)
 
 

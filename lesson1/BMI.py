d7665752e0751fa1d27f8c00c99ed3d8d443d4f2
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 13:53:07 +0800

    Python

--- lesson1/BMI.py
+++ lesson1/BMI.py
@@ -9,4 +9,5 @@ print("身高: %d 體重: %d BMI: %.2f" % (h, w, bmi))
 print("身高: %06d 體重: %06d BMI: %06.2f" % (h, w, bmi))
 result = "身高: {0} 體重: {1} BMI: {2}".format(h, w, bmi)
 print(result)
-
+result = "身高: {0} 體重: {1} BMI: {2}".format(h, w, ("%.2f" % bmi))
+print(result)

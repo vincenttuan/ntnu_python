a9d1ae55de32cf699aebfd19eed23c918a81c355
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 15:22:05 +0800

    Python

--- lesson1/Lotto.py
+++ lesson1/Lotto.py
@@ -4,5 +4,14 @@ a = random.randint(0, 9)
 b = random.randint(0, 9)
 c = random.randint(0, 9)
 d = random.randint(0, 9)
-
 print(a, b, c, d)
+
+for i in range(4):
+    num = random.randint(0, 9)
+    print(i, num)
+
+r1 = range(4)
+for i in r1:
+    num = random.randint(0, 9)
+    print(i, num)
+

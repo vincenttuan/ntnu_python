6c3dc5b7b25e69161b01ab5abf77676ecbb255d8
CLASS <vincentjava@yahoo.com.tw>
2020-01-20 14:23:17 +0800

    Python

--- lesson1/BMI_Input.py
+++ lesson1/BMI_Input.py
@@ -6,3 +6,16 @@ print(type(w))
 bmi = w / ((h/100)**2)
 result = "身高: {0} 體重: {1} BMI: {2}".format(h, w, ("%.2f" % bmi))
 print(result)
+
+info = '正常'
+if bmi > 23:
+    print('過重')
+    info = '過重'
+elif bmi < 18:
+    print('過輕')
+    info = '過輕'
+else:
+    print('正常')
+
+result = "身高: {0} 體重: {1} BMI: {2} {3}".format(h, w, ("%.2f" % bmi), info)
+print(result)

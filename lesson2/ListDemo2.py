0296ace2eb1b6d6aac8deca95788e923f8fe8fde
CLASS <vincentjava@yahoo.com.tw>
2020-01-22 13:37:25 +0800

    Python

---
+++ lesson2/ListDemo2.py
@@ -0,0 +1,18 @@
+import random
+dice = [1, 2, 3, 4, 5, 6]
+
+def getDice():
+    index = random.randint(0, 5)
+    print(dice[index])
+    return dice[index]
+
+def play(n):
+    sum = 0
+    for i in range(n):
+        sum = sum + getDice()
+    return sum
+
+score = play(3)
+print('score: %d' % score)
+
+

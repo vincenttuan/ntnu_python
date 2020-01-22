7762899120e8030d97f4305e73b0a9496350dc39
CLASS <vincentjava@yahoo.com.tw>
2020-01-22 10:29:41 +0800

    Python

--- lesson2/GuessNumber.py
+++ lesson2/GuessNumber.py
@@ -1,7 +1,15 @@
-ans = 74
+import random
+
+begin = 0
+end = 100
+ans = random.randint(begin+1, end-1)
+
 while 1:
-    guess = int(input('請在 0 ~ 100 間輸入數字'))
+    guess = int(input('請在 %d ~ %d 間輸入數字: ' % (begin, end)))
     if guess == ans:
         print('答對了')
         break
-
+    elif guess < ans:
+        begin = guess
+    else:
+        end = guess

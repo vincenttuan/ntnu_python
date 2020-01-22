26b9aa59f80880808cd6747412e8e7b0e36cc092
CLASS <vincentjava@yahoo.com.tw>
2020-01-22 15:19:31 +0800

    Python

--- lesson2/JsonDemo.py
+++ lesson2/JsonDemo.py
@@ -1,9 +1,10 @@
 import json
 
 x1 = '{"name":"John", "age":30}'
-y1 = json.loads(x1)
+y1 = json.loads(x1) # 文字轉 json 物件
 print(type(y1))
 print(y1)
+print(y1['name'])
 
 x2 = '[{"name":"John", "age":30},' \
      '{"name":"Mary", "age":20}]'

8cb094b91cc4e1853d8f532bf260c40969a1e215
CLASS <vincentjava@yahoo.com.tw>
2020-01-31 14:41:21 +0800

    Python

--- lesson3/PM25.py
+++ lesson3/PM25.py
@@ -5,4 +5,13 @@ url = 'https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=10'
 
 resp = requests.get(url)
 html = resp.text
-print(html)
+#print(html)
+
+sp = BeautifulSoup(html, 'html.parser')
+
+area = sp.find('a', {"id":"ctl08_gv_ctl20_linkSite"}).text.strip()
+p1 = sp.find('span', {"id":"ctl08_gv_ctl20_lab1"}).text.strip()
+p2 = sp.find('span', {"id":"ctl08_gv_ctl20_lab2"}).text.strip()
+print(area, p1, p2)
+
+print(sp.find('span', {"id":"ctl08_gv_ctl04_lab1"}).text.strip())

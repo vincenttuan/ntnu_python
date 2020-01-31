aefab80ef6f27a73da73ca351520661d0510e2ec
CLASS <vincentjava@yahoo.com.tw>
2020-01-31 14:57:34 +0800

    Python

--- lesson3/PM25.py
+++ lesson3/PM25.py
@@ -15,3 +15,11 @@ p2 = sp.find('span', {"id":"ctl08_gv_ctl20_lab2"}).text.strip()
 print(area, p1, p2)
 
 print(sp.find('span', {"id":"ctl08_gv_ctl04_lab1"}).text.strip())
+
+# 抓取所有資料
+records = sp.find_all('tr', {"align":"center", "style":"border-width:1px;border-style:Solid;"})
+for record in records:
+    td = record.find_all('td')
+    for i in range(0, 3):
+        print(td[i].text.strip(), end=",")
+    print('\n--------------------')

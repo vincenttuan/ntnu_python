0220204d5eaf2f879125b1ad638c571c16eb08bd
CLASS <vincentjava@yahoo.com.tw>
2020-02-03 10:16:56 +0800

    Python

--- lesson4/Win_3.py
+++ lesson4/Win_3.py
@@ -1,5 +1,5 @@
 import tkinter
-from datetime import datetime
+import random
 
 num = 0;
 
@@ -8,6 +8,14 @@ def update_number():
     num = num + 1
     print(str(num))
     var.set(str(num))
+
+def update_lotto():
+    lottos = set()
+    while len(lottos) < 5:
+        lottos.add(random.randint(1, 39))
+
+    print(tuple(lottos))
+    var.set(tuple(lottos))
 
 win = tkinter.Tk()
 # 視窗抬頭
@@ -23,6 +31,9 @@ label.pack(side=tkinter.TOP)
 
 button1 = tkinter.Button(win, text="GetNumber", font=('Arial', 20), command=update_number)
 button1.pack(side=tkinter.LEFT)
+
+button2 = tkinter.Button(win, text="539", font=('Arial', 20), command=update_lotto)
+button2.pack(side=tkinter.RIGHT)
 
 win.mainloop()
 

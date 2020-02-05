d032c7b7df57b67a750d2e6d51204f5bfeaa30e4
CLASS <vincentjava@yahoo.com.tw>
2020-02-05 13:05:55 +0800

    Python

---
+++ lesson5/OpenCV_Detection_LicensePlate.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-
+import cv2
+
+carPath = "./xml2/tw.xml"
+carCascade = cv2.CascadeClassifier(carPath)
+
+img = cv2.imread("./image/car3.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cars = carCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=8,
+    minSize=(60, 60),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+# 畫出車牌
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+
+cv2.imshow('LicensePlate?', img)
+# 儲存檔案
+# cv2.imwrite("./image_out/LicensePlate.jpg",img)
+# 任意鍵離開(若設定為 0 就表示持續等待至使用者按下按鍵為止)
+c = cv2.waitKey(0)
